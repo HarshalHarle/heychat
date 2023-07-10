e4b0b1c033c3d8aa7a7eda06a1c97d046571c94f
Harshal Harle <34764788+HarshalHarle@users.noreply.github.com>
2023-07-10 10:01:39 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+import AuthContext from './context/AuthContext'
+import ActiveStatus from './components/ActiveStatus'
+import ToasterContext from './context/ToasterContext'
+
+export const metadata = {
+  title: 'HeyChat Messenger',
+  description: 'HeyChat Messenger',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  )
+}
